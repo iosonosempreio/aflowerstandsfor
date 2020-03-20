a98f40aed5296b33371a2cd7e5beb3f79de4ec34
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-03-20 16:26:42 -0400

    Updates

---
+++ precache-manifest.047436efecb0eeb0ee8138ea403e4335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6426808d1d194ed4114c822719094a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7daf71f40c4b23b0a27",
+    "url": "./static/css/main.0d3a0996.chunk.css"
+  },
+  {
+    "revision": "5abef54cfce78e107859",
+    "url": "./static/js/2.b8acd7c5.chunk.js"
+  },
+  {
+    "revision": "9124f3d4d4d1706c8b2193315ce09509",
+    "url": "./static/js/2.b8acd7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7daf71f40c4b23b0a27",
+    "url": "./static/js/main.7a79f607.chunk.js"
+  },
+  {
+    "revision": "ae68d06a9e1b696b2281",
+    "url": "./static/js/runtime-main.9a7d6a7a.js"
+  }
+]);
