1c51f7dee3b18da15d915a1bd9288316cd9a4df7
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-03-30 17:26:30 -0400

    performances tests

---
+++ src/PIXIPerformancesTest/PIXIPerformancesTest.js
@@ -0,0 +1,145 @@
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import * as PIXI from "pixi.js";
+import { Viewport } from "pixi-viewport";
+
+const dpr = window.devicePixelRatio || 1;
+
+class PIXIPerformancesTest extends Component {
+
+  componentDidMount(){
+    const width = window.innerWidth / dpr;
+    const height = window.innerHeight / dpr;
+
+    let ticker = PIXI.Ticker.shared;
+    ticker.stop();
+
+    const app = new PIXI.Application({
+      autoStart:false,
+      width: width,
+      height: height,
+      backgroundColor: 0xf4fff1,
+      resolution: dpr,
+      powerPreference: 'high-performance',
+      sharedTicker: false
+    });
+    this._rootNode.appendChild(app.view);
+
+    const viewport = new Viewport({
+      // center: new PIXI.Point(-width/2, -height/2),
+      passiveWheel: false,
+      stopPropagation: true,
+      divWheel: this._rootNode,
+      screenWidth: width,
+      screenHeight: height,
+      worldWidth: width,
+      worldHeight: height,
+      interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    });
+    viewport
+      // .clampZoom(Utilities.clampZoomOptions)
+      .drag({pressDrag:true, clampWheel:true})
+      .pinch()
+      .wheel()
+    app.stage.addChild(viewport);
+
+    const amount = 200000;
+    const container = new PIXI.ParticleContainer(amount, {
+      vertices: false,
+      position: true,
+      rotation: false,
+      uvs: false,
+      tint: false
+    });
+
+    viewport.addChild(container);
+
+    const textures = []
+    
+    app.loader.add('sprites', './flowers-textures-1.png');
+    app.loader.onProgress.add((e)=>{
+      console.log(e.progress+'%');
+    })
+    app.loader.onComplete.add(async ()=>{
+      console.log('loader completed')
+      const baseTexture = app.loader.resources.sprites.texture.baseTexture;
+
+      const flowers_textures_info = await d3.json('./flowers-textures-1.json');
+
+      for (let texture_name in flowers_textures_info.frames) {
+        const frame = flowers_textures_info.frames[texture_name].frame;
+        const texture = new PIXI.Texture(
+          baseTexture,
+          new PIXI.Rectangle(frame.x, frame.y, frame.w, frame.h)
+        );
+        textures.push(texture);
+      }
+
+      for (let i=0; i<amount; ++i) {
+        const index_texture = i%5;
+        const sprite = new PIXI.Sprite(textures[index_texture]);
+  
+        sprite.speedX = (-0.5 + Math.random());
+        sprite.speedY = (-0.5 + Math.random());
+      
+        sprite.anchor.x = 0.5;
+        sprite.x = width*Math.random();
+        sprite.y = height*Math.random();
+        sprite.scale.x = 1/2/2/2;
+        sprite.scale.y = 1/2/2/2;
+  
+        container.addChild(sprite);
+      }
+
+      let counter = 0;
+      const update = function () {          
+        if (counter < 1000) {
+          counter++;
+          for (var i = 0; i < amount; i++) 
+          {
+            var sprite = container.children[i];
+            sprite.x += sprite.speedX;
+            sprite.y += sprite.speedY;
+            
+            if (sprite.x > width)
+            {
+              sprite.speedX *= -1;
+              sprite.x = width;
+            }
+            else if (sprite.x < 0)
+            {
+              sprite.speedX *= -1;
+              sprite.x = 0;
+            }
+            // y
+            if (sprite.y > height)
+            {
+              sprite.speedY *= -1;
+              sprite.y = height;
+            }
+            else if (sprite.y < 0)
+            {
+              sprite.speedY *= -1;
+              sprite.y = 0;
+            }
+          }
+        };
+        
+        app.renderer.render(viewport);
+        requestAnimationFrame(update);
+      }
+      update();
+
+    })
+    app.loader.load();
+  }
+  
+  _setRef(componentNode) {
+    this._rootNode = componentNode;
+  }
+  render() {
+    return <div ref={this._setRef.bind(this)}></div>;
+  }
+}
+
+export default PIXIPerformancesTest;
