a5f915bc52cf88aeee3ddc065c6f8d600f41b49f
iosonosempreio <iosonosempreio@users.noreply.github.com>
2020-04-01 23:38:46 -0400

    country based component for viz

--- src/Italy/Italy.js
+++ src/Italy/Italy.js
@@ -13,26 +13,27 @@ class Italy extends Component {
   async componentDidMount(){
     const fetched = await Promise.all([
       d3.csv('./data/italy/datasets.csv'),
-      d3.json('./data/regioni.geojson')
+      d3.json('./data/world-50m.json')
     ]);
     const datasets = await Promise.all(fetched[0].map(d=>d3.csv('./data/italy/'+d.file_name)));
     const data = {};
     fetched[0].map(d=>d.date).forEach((date,i)=>{
       data[date] = datasets[i];
     })
-    const map = fetched[1];
-    this.setState({data:data,map:map});
+    const topojson = fetched[1];
+    this.setState({data:data,topojson:topojson});
   }
   render() {
     return <div className="italy" ref={this._setRef.bind(this)}>
       {!this.state.data && <div>Loading data...</div>}
-      {this.state.data && <Visualization data={this.state.data} map={this.state.map} country_info={country_info}/>}
+      {this.state.data && <Visualization data={this.state.data} topojson={this.state.topojson} country_info={country_info}/>}
     </div>;
   }
 }
 export default Italy;
 
 const country_info = {
+  'country_code':'380',
   'regions_codes':{
     '13':'Abruzzo',
     '17':'Basilicata',
--- src/Visualization/Visualization.js
+++ src/Visualization/Visualization.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import * as PIXI from "pixi.js";
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
 import { Viewport } from "pixi-viewport";
 import Utilities from '../Utilities/Utilities';
 
-let app, viewport, flowersContainer, geographyContainer, textures,
+let app, viewport, flowersContainer, geography, textures,
     dpr=window.devicePixelRatio || 1,
     width=window.innerWidth,
     height=window.innerHeight,
     available_dates,
-    projection,
+    projection = d3.geoNaturalEarth1(),
+    path = d3.geoPath(),
     updateQueue=[],
     prevTime=0;
 
@@ -46,7 +49,7 @@ class Visualization extends Component {
     app = new PIXI.Application({
       width: width,
       height: height,
-      backgroundColor: 0xf4fff1,
+      backgroundColor: 0xfafafa,
       autoDensity:true,
       resolution: dpr,
       powerPreference: 'high-performance',
@@ -78,13 +81,23 @@ class Visualization extends Component {
     app.stage.addChild(viewport);
 
     // Initialize containers
-    geographyContainer = new PIXI.Container();
-    const shapes = new PIXI.Graphics();
-    viewport.addChild(shapes);
-    shapes.beginFill(0x000000,1)
-    shapes.drawCircle(0,0,10);
-    shapes.endFill();
-    viewport.addChild(geographyContainer);
+    geography = new PIXI.Container();
+    viewport.addChild(geography);
+    const topoWorld = this.props.topojson;
+    // contries codes https://en.wikipedia.org/wiki/ISO_3166-1_numeric
+    let world = topojson.feature(topoWorld, topoWorld.objects.countries);
+    const country = world.features.find(d=>d.id===this.props.country_info.country_code)
+    projection.fitExtent([[5,5],[768-10,1024-10]], country);
+    world.features.filter(d=>d.id===this.props.country_info.country_code).forEach(feature=>{
+      const this_graphic = new PIXI.Graphics();
+      path.projection(projection).context(this_graphic);
+      this_graphic.beginFill(0xdfffd6, 1);
+      this_graphic.lineStyle(1, 0xfafafa, 0.5);
+      path(feature);
+      this_graphic.endFill();
+      geography.addChild(this_graphic);
+    });
+    
     flowersContainer = new PIXI.ParticleContainer(unique_IDS.length, {
       vertices: false,
       position: true,
@@ -93,7 +106,6 @@ class Visualization extends Component {
       tint: false
     });
     viewport.addChild(flowersContainer);
-    app.renderer.render(viewport);
 
     app.loader.add('flowersTextures','./flowers-textures-1.json');
     app.loader.onComplete.add(()=>{
@@ -107,8 +119,8 @@ class Visualization extends Component {
           sprite.y = 50;
           sprite.anchor.x = 0.5;
           sprite.anchor.y = 0.5;
-          sprite.scale.x = 1/40;
-          sprite.scale.y = 1/40;
+          sprite.scale.x = 1/40 * 8;
+          sprite.scale.y = 1/40 * 8;
           tempSpritesList[flower_id]=sprite;
         }
 
@@ -131,18 +143,29 @@ class Visualization extends Component {
       });
     app.loader.load();
   }
-  componentDidUpdate(prevProps,prevState){
-    if (prevProps.day_index !== this.state.day_index) {
+  componentDidUpdate(prevProps, prevState){
+    console.log('did update')
+    if (prevState.day_index !== this.state.day_index) {
       //update draw list
+      console.log('day')
       this.state.viewBuffer.clearObjects();
       this.state.viewBuffer.setDrawList(this.props.data[available_dates[this.state.day_index]], this.state.model);
       this.state.viewBuffer.drawObjects();
     } else if (prevState.model !== this.state.model) {
       //animate moveto see ViewBuffer startAnimation()
-      this.state.viewBuffer.model = this.props.model;
+      console.log('animation')
+      this.state.viewBuffer.model = this.state.model;
       this.state.viewBuffer.startAnimation(5, app);
+    } else {
+      console.log('none of above')
     }
-    // console.log(container);
+    // if (this.state.model === 'stripes') {
+    //   geography.renderable = false;
+    //   viewport.snap(0, 0, {topLeft:true,interrupt:true,removeOnComplete:true,removeOnInterrupt:true});
+    //   viewport.snapZoom({width: width, interrupt:true, removeOnComplete: true, removeOnInterrupt: true});
+    // } else {
+    //   geography.renderable = true;
+    // }
   }
   changeDate(index){
     if (index<0) {
@@ -222,6 +245,7 @@ class ViewBuffer{
    * @param {PIXI.Application} app 
    */
   startAnimation(animationTime, app){
+    console.log('animation')
     this.animationTime = animationTime;
     this.timeLeft = 0;
     this.app = app;
